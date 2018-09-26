6012e833925db8a85bd3972d17c4160672bbd520
JAho <jere.v.aho@eng.tamk.fi>
2018-09-26 13:09:56 +0300

    react 2a valmis

---
+++ src/components/Countries.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import countries from '../countries';
+
+interface ICountriesState {
+}
+
+
+class Countries extends React.Component<{}, ICountriesState> {
+  public render() {
+    return (
+        <Paper>
+          <h1>Countries</h1>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  Name
+                </TableCell>
+                <TableCell>
+                  Population
+                </TableCell>
+                <TableCell>
+                  Percentage
+                </TableCell>
+                <TableCell>
+                  Position
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {countries.map(country =>
+                (<TableRow>
+                  <TableCell>
+                    {country.name}
+                  </TableCell>
+                  <TableCell>
+                    {country.population}
+                  </TableCell>
+                  <TableCell>
+                    {country.percentage}
+                  </TableCell>
+                  <TableCell>
+                    {country.position}
+                  </TableCell>
+                </TableRow>))}
+            </TableBody>
+          </Table>
+        </Paper>
+    )
+  };
+}
+
+export default Countries;
